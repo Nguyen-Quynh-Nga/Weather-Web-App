f15c23bec8d20a7bb4cf1a56150cf248d22f8815
1801040158@s.hanu.edu.vn <1801040158@s.hanu.edu.vn>
2021-11-15 23:03:28 +0700

    fix code

--- Weather Web App/weather.js
+++ Weather Web App/weather.js
@@ -10,188 +10,167 @@ const hourlyForecast=document.querySelector(".hourly-forecast")
 const currentContainer=document.querySelector(".current-container")
 const searchInput=document.querySelector('#search-input')
 const container= document.querySelector(".container")
+const hourSubContainer = document.querySelector(".hour-subcontainer")
+
+
+btnBack=document.createElement("button")
+    btnBack.className="btnBack button"
+    btnBack.innerHTML="Back"
+    hourSubContainer.appendChild(btnBack) 
+    hourlyForecast.appendChild(btnBack)
+
+    btnNext=document.createElement("button")
+    btnNext.className="btnNext button"
+    btnNext.innerHTML="Next"
+    hourSubContainer.appendChild(btnNext)
+    hourlyForecast.appendChild(btnNext)
+
+    btnBack=document.querySelector(".btnBack")
+    btnNext=document.querySelector(".btnNext")
+
+    var curDate= new Date()
+    var index= curDate.getHours()
+    console.log(index)
 
 
 
-
-async function getAPI(url = 'https://weatherapi-com.p.rapidapi.com/forecast.json?q=Hanoi&days=5') {
+async function getAPI(city) {
    
-    
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-       
-        "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
-        "x-rapidapi-key": "eaea6017f3msh7a918588b36e934p15386bjsnffe896452576"
-      },
-    });
-    return response.json(); 
-    
+  url = `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${city}&days=5`
+  console.log(url)
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+     
+      "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+      "x-rapidapi-key": "eaea6017f3msh7a918588b36e934p15386bjsnffe896452576"
+    },
+  });
+  return response.json(); 
+  
+}
+
+function createObj(hour,temperature,imgSrc,percent){
+      subsubcontainer= document.createElement("div");
+      subsubcontainer.className="sub-sub-container"
+      hourP=document.createElement("p")
+      hourP.innerHTML=hour
+      temperatureP=document.createElement("p")
+      temperatureP.innerHTML=temperature
+      imgTag= document.createElement("img")
+      imgTag.src=imgSrc
+      percentP=document.createElement("p")
+      percentP.innerHTML=percent
+      space= document.createElement("div")
+      space.className='space'
+      subsubcontainer.appendChild(hourP)
+      subsubcontainer.appendChild(temperatureP)
+      subsubcontainer.appendChild(imgTag)
+      subsubcontainer.appendChild(percentP)
+      subsubcontainer.appendChild(space)
+      
+      return subsubcontainer
+
+}
+
+function render(newHourList){
+  hourSubContainer.innerHTML = "";
+  for(hour of newHourList){
+    createObj(hour.time.slice(-5),hour.temp_c+" C",(Object.values(hour.condition))[1],hour.chance_of_rain+"%")
+    hourSubContainer.appendChild(subsubcontainer)
+    hourlyForecast.appendChild(hourSubContainer)
+}
+  
 }
 
 
-
-
-getAPI().then(result =>{
+getAPI('ha noi').then(result =>{
     console.log(result)
-    const j=result.forecast.forecastday;
-
+   
     cityName.innerHTML=result.location.name;
     weatherState.innerHTML=(Object.values(result.current.condition))[0];
     weatherIcon=document.createElement('img')
     weatherIcon.src=(Object.values(result.current.condition))[1]
     currentContainer.appendChild(weatherIcon)
     currentTemp.innerHTML=result.current.temp_c + " " + "C";
-
     humidity.innerHTML=result.current.humidity+ "%"
     wind.innerHTML=result.current.wind_mph+ "km/h"
     uv.innerHTML=result.current.uv
 
-    h3Daily=document.createElement('h3')
-    h3Daily.innerHTML='Daily Forecast'
-    dailyForecast.appendChild(h3Daily)
+    hourArr = result.forecast.forecastday[0].hour
+    console.log(hourArr)
 
-    for(let i of j){
-     
-      divDaily=document.createElement('div')
-      divDaily.className="div-daily"
-      day=document.createElement('p')
-      temperature=document.createElement('p')
-      icon=document.createElement('img')
-      icon.src=(Object.values(i.day.condition))[1]
-      percent=document.createElement('p')
-      day.innerHTML=i.date;
-      temperature.innerHTML=i.day.avgtemp_c + " C"
-      percent.innerHTML=i.day.daily_chance_of_rain+"%";
-      divDaily.appendChild(day)
-      divDaily.appendChild(temperature)
-      divDaily.appendChild(icon)
-      divDaily.appendChild(percent)
-      dailyForecast.appendChild(divDaily)
-}
-    let k=j[0].hour;
-    // console.log(k)
-    h3Hourly=document.createElement('h3')
-    h3Hourly.innerHTML="Hourly Forecast"
-    hourlyForecast.appendChild(h3Hourly);
+    btnBack.addEventListener("click", () => {
+      index -=1;
+      if(index <0){
+        index = 20;
+      }
+      console.log(index);
+      newHourList = hourArr.slice(index, index+4)
+      render(newHourList)
+})
 
-    for( let x=0;x<k.length;x++){
-      // console.log(k[x].time)
-      divHourly=document.createElement("div")
-      divHourly.className="div-hourly"
-      hour=document.createElement('p')
-      temHourly=document.createElement('p')
-      iconHourly=document.createElement('img')
-      iconHourly.src=(Object.values(k[x].condition))[1]
-      percentHourly=document.createElement('p')
-      hour.innerHTML=k[x].time.slice(-5)
-      temHourly.innerHTML=k[x].temp_c+" C"
-      percentHourly.innerHTML=k[x].chance_of_rain+"%"
-      divHourly.appendChild(hour)
-      divHourly.appendChild(temHourly)
-      divHourly.appendChild(iconHourly)
-      divHourly.appendChild(percentHourly)
-      hourlyForecast.appendChild(divHourly)
+    btnNext.addEventListener("click", () => {
+      index +=1;
+      if(index >20 ){
+        index = 0;
+        
+      }
+      console.log(index);
+      newHourList = hourArr.slice(index, index+4)
+      render(newHourList)
+})
+render(hourArr.slice(index, index+4))
+
+dayArr= result.forecast.forecastday;
+    // console.log(dayArr)
+    dailySubContainer=document.querySelector(".daily-subcontainer")
+    for(dayly of dayArr){
+
+      createObj(dayly.date,dayly.day.avgtemp_c+" C",dayly.day.condition.icon,dayly.day.daily_chance_of_rain+"%")
+      dailySubContainer.appendChild(subsubcontainer)
+      dailyForecast.appendChild(dailySubContainer)
     }
-  })
-// .catch(
-//   console.log("error")
-// )
+})
+
+
+
 
 searchInput.addEventListener("change", async ()=> {
-
-  // let del=await fetch('https://weatherapi-com.p.rapidapi.com/forecast.json?q=Hanoi&days=5',{
-  //   method: "DELETE"
-
-  //     }).then(del =>{
-  //       console.log(del.status);
-  //     })
-    
-
-
-
   let userInput= document.getElementById('search-input').value;
   console.log(userInput)
-  
-  
-  let res= await fetch("https://weatherapi-com.p.rapidapi.com/forecast.json?q=**"+ userInput +"**&days=3",{
-    method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-       
-        "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
-        "x-rapidapi-key": "eaea6017f3msh7a918588b36e934p15386bjsnffe896452576"
-      },
-
-  })
-  await res.json().then(res =>{
+  getAPI(userInput).then(res=>{
     console.log(res)
-    const j=res.forecast.forecastday;
-    // console.log(j)
     cityName.innerHTML=res.location.name;
     weatherState.innerHTML=(Object.values(res.current.condition))[0];
-    // weatherIcon=document.createElement('img')
     weatherIcon.src=(Object.values(res.current.condition))[1]
-    // currentContainer.appendChild(weatherIcon)
     currentTemp.innerHTML=res.current.temp_c + " " + "C";
-
     humidity.innerHTML=res.current.humidity+ "%"
     wind.innerHTML=res.current.wind_mph+ "km/h"
     uv.innerHTML=res.current.uv
 
-    // h3Daily=document.createElement('h3')
-    // h3Daily.innerHTML='Daily Forecast'
-    // dailyForecast.appendChild(h3Daily)
+    hourArr = res.forecast.forecastday[0].hour
+    
+    console.log(hourArr)
+    
+    hourSubContainer.innerHTML=""
+    render(hourArr.slice(index, index+4))
 
-    for(let i of j){
-     
-      // divDaily=document.createElement('div')
-      // divDaily.className="div-daily"
-      // day=document.createElement('p')
-      // temperature=document.createElement('p')
-      // icon=document.createElement('img')
-      icon.src=(Object.values(i.day.condition))[1]
-      // percent=document.createElement('p')
-      day.innerHTML=i.date;
-      temperature.innerHTML=i.day.avgtemp_c + " C"
-      percent.innerHTML=i.day.daily_chance_of_rain+"%";
-      // divDaily.appendChild(day)
-      // divDaily.appendChild(temperature)
-      // divDaily.appendChild(icon)
-      // divDaily.appendChild(percent)
-      // dailyForecast.appendChild(divDaily)
-}
-    let k=j[0].hour;
-    // console.log(k)
-    // h3Hourly=document.createElement('h3')
-    h3Hourly.innerHTML="Hourly Forecast"
-    // hourlyForecast.appendChild(h3Hourly);
+    dayArr= res.forecast.forecastday;
+    console.log(dayArr)
+    dailySubContainer=document.querySelector(".daily-subcontainer")
+    dailySubContainer.innerHTML=""
+    for(dayly of dayArr){
 
-    for( let x=0;x<k.length;x++){
-      // console.log(k[x].time)
-      // divHourly=document.createElement("div")
-      // divHourly.className="div-hourly"
-      // hour=document.createElement('p')
-      // temHourly=document.createElement('p')
-      // iconHourly=document.createElement('img')
-      iconHourly.src=(Object.values(k[x].condition))[1]
-      // percentHourly=document.createElement('p')
-      hour.innerHTML=k[x].time.slice(-5)
-      temHourly.innerHTML=k[x].temp_c+" C"
-      percentHourly.innerHTML=k[x].chance_of_rain+"%"
-      // divHourly.appendChild(hour)
-      // divHourly.appendChild(temHourly)
-      // divHourly.appendChild(iconHourly)
-      // divHourly.appendChild(percentHourly)
-      // hourlyForecast.appendChild(divHourly)
-    }
+      createObj(dayly.date,dayly.day.avgtemp_c+" C",dayly.day.condition.icon,dayly.day.daily_chance_of_rain+"%")
+      dailySubContainer.appendChild(subsubcontainer)
+      dailyForecast.appendChild(dailySubContainer)
+   }
   })
-      // container.classList.add('hidden')
-      // container.classList.remove('hidden')
 })
 
 
-  
+
 
 
